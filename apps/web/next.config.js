e35488e5cb394b7a95b77bd84605f216ea001671
fujipug <lane.fujikado@gmail.com>
2023-08-24 12:22:54 -0600

    set everything

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,25 +1,27 @@
+const { SWOP_URL } = process.env;
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   images: {
     domains: ["pickasso.nyc3.cdn.digitaloceanspaces.com"],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: `/:path*`,
-  //     },
-  //     {
-  //       source: '/swop',
-  //       destination: 'https://byquokkas-swop.vercel.app/swop',
-  //     },
-  //     {
-  //       source: '/swop/:path*',
-  //       destination: 'https://byquokkas-swop.vercel.app/swop/:path*',
-  //     }
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/swop',
+        destination: `${SWOP_URL}.vercel.app/swop`,
+      },
+      {
+        source: '/swop/:path*',
+        destination: `${SWOP_URL}.vercel.app/swop/:path*`,
+      }
+    ]
+  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
