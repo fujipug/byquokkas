63d829475f61783899f51fb18a9870ca103ac8f1
fujipug <lane.fujikado@gmail.com>
2023-08-24 12:13:34 -0600

    remove config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,22 +4,22 @@ module.exports = {
   images: {
     domains: ["pickasso.nyc3.cdn.digitaloceanspaces.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `/:path*`,
-      },
-      {
-        source: '/swop',
-        destination: 'https://byquokkas-swop.vercel.app/swop',
-      },
-      {
-        source: '/swop/:path*',
-        destination: 'https://byquokkas-swop.vercel.app/swop/:path*',
-      }
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination: `/:path*`,
+  //     },
+  //     {
+  //       source: '/swop',
+  //       destination: 'https://byquokkas-swop.vercel.app/swop',
+  //     },
+  //     {
+  //       source: '/swop/:path*',
+  //       destination: 'https://byquokkas-swop.vercel.app/swop/:path*',
+  //     }
+  //   ]
+  // },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
