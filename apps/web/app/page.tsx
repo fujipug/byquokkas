32a852f5581077d8d3fdbf2ab7b1c7529ff07fda
fujipug <lane.fujikado@gmail.com>
2023-08-24 14:11:37 -0600

    save

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Table from "ui/Table";
 import { Offer } from "../../swop/types";
 import { getPublicOffers } from "../../../apis/swop";
+import Link from "next/link";
 
 const swopTableHeaders = ['Nfts Offered', 'Offer Name', 'Creator', 'Status', 'Created At'];
 const rabbleTableHeaders = ['Nfts Offered', 'Offer Name', 'Creator', 'Status', 'Created At'];
@@ -22,13 +23,11 @@ export default function Page() {
     <>
       <h1 className="text-2xl sm:text-3xl text-center">Welcome to ByQuokkas</h1>
 
-      <div className="p-6 bg-neutral rounded-box flex justify-center my-8">
-        <span className="text-3xl sm:text-5xl">RABBLE RABBLE</span>
-      </div>
+      <a href="https://rabblerabble.xyz">
+        <button className="btn btn-lg rounded-2xl bg-neutral text-5xl w-full pt-5 pb-16 my-8">RABBLE RABBLE</button>
+      </a>
 
-      <div className="p-6 bg-neutral rounded-box flex justify-center">
-        <span className="text-3xl sm:text-5xl">SWOP</span>
-      </div>
+      <Link href="/swop" className="btn btn-lg rounded-2xl bg-neutral text-5xl w-full pt-5 pb-16">SWOP</Link>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 mt-12">
         <div className="cols-span-1">
