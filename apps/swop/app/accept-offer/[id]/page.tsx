cf05a11812a2c30c4dbd408e70b6b1da59e66aed
fujipug <lane.fujikado@gmail.com>
2023-08-23 21:25:27 -0600

    accept offer updates

--- apps/swop/app/accept-offer/[id]/page.tsx
+++ apps/swop/app/accept-offer/[id]/page.tsx
@@ -7,13 +7,19 @@ import Image from 'next/image'
 import { useContractWrite } from 'wagmi';
 import { useFee, useSwopContract } from '../../../../../utils/hooks';
 import { swopContractAbi } from '../../../../../packages/swop-config';
-import { getSwapId } from '../../../../../utils/contract-funtions';
+import { getSwapId, verifyApproval } from '../../../../../utils/contract-funtions';
+import { doc, getFirestore, updateDoc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../../../../../packages/firebase-config';
 
 declare global {
   interface Window {
     nftInfoModal: any;
   }
 }
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default function AcceptOffer({ params }: { params: { id: string } }) {
   const [offer, setOffer] = useState<any>(null);
@@ -30,7 +36,7 @@ export default function AcceptOffer({ params }: { params: { id: string } }) {
     onSuccess: (res: any) => {
       // TODO: Call read to get swapId
       console.log('sucess: ', res);
-      // createFirebaseOffer();
+      updateFirebaseOffer();
     },
     onError(error) {
       // Display Error Message
@@ -44,13 +50,29 @@ export default function AcceptOffer({ params }: { params: { id: string } }) {
 
     getSwapId().then((swapId) => {
       console.log('swapId: ', swapId);
-      setSwapId(swapId);
+      setSwapId(Number(swapId));
     });
   }, [params.id]);
 
   const handleInfoModal = (info: any) => {
     setNftInfoModal(info);
     window.nftInfoModal.showModal();
+  }
+
+  // Finalize oand accept offer
+  const [isApprovalLoading, setIsApprovalLoading] = useState(false);
+  const handleAcceptOffer = async () => {
+    const collectionBAddresses = offer.offerB.map((offer: any) => offer.collectionAddress);
+    verifyApproval(collectionBAddresses, write, (isApprovalStatusLoading: boolean) => {
+      setIsApprovalLoading(isApprovalStatusLoading);
+    });
+  }
+
+  const updateFirebaseOffer = async () => {
+    const offerRef = doc(db, 'offers', offer.id);
+    updateDoc(offerRef, {
+      status: 'Closed',
+    });
   }
 
   return (
@@ -71,7 +93,7 @@ export default function AcceptOffer({ params }: { params: { id: string } }) {
         <OfferContainer active={false} offers={offer?.offerB} placeholderText='Loading ...' showRemove={false} onSelectedNftEmit={handleInfoModal} />
       </div>
       <div className='flex justify-evenly items-center'>
-        <button className='btn btn-secondary'>
+        <button onClick={() => handleAcceptOffer} className='btn btn-secondary'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
           </svg>
