f00efcecb5a463feb770eb94f7b540801b4ad9c2
fujipug <lane.fujikado@gmail.com>
2023-08-29 22:30:18 -0600

    counter offer console func

--- apps/swop/app/accept-offer/[id]/page.tsx
+++ apps/swop/app/accept-offer/[id]/page.tsx
@@ -94,7 +94,7 @@ export default function AcceptOffer({ params }: { params: { id: string } }) {
   }
 
   const handleCounterOffer = async () => {
-
+    console.log('counter offer');
   }
 
   return (
